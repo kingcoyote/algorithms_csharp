27610ba478ff8af4d9b3b8302ccc2de1e3c51858
Steve Phillips <kingcoyote85@gmail.com>
2021-12-13 06:20:38 -0800

    Creating getters for id and size fields.

--- CC.cs
+++ CC.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 namespace Algorithms {
     public class CC {
-        private bool[] marked;   // marked[v] = has vertex v been marked?
-        public int[] id;           // id[v] = id of connected component containing v
-        private int[] size;         // size[id] = number of vertices in given component
-        public int count {get; private set;}          // number of connected components
+        public int count { get; private set; }          // number of connected components
+
+        private bool[] _marked;   // marked[v] = has vertex v been marked?
+        private int[] _id;           // id[v] = id of connected component containing v
+        public int[] _size;         // size[id] = number of vertices in given component
 
         public CC(Graph graph) {
-            marked = new bool[graph.Vertices];
-            id = new int[graph.Vertices];
-            size = new int[graph.Vertices];
+            _marked = new bool[graph.Vertices];
+            _id = new int[graph.Vertices];
+            _size = new int[graph.Vertices];
             for (int v = 0; v < graph.Vertices; v++) {
-                if (!marked[v]) {
+                if (!_marked[v]) {
                     dfs(graph, v);
                     count++;
                 }
@@ -22,18 +23,29 @@ namespace Algorithms {
 
         // depth-first search for a Graph
         private void dfs(Graph graph, int v) {
-            marked[v] = true;
-            id[v] = count;
-            size[count]++;
+            _marked[v] = true;
+            _id[v] = count;
+            _size[count]++;
             foreach (int w in graph.adj(v)) {
-                if (!marked[w]) {
+                if (!_marked[w]) {
                     dfs(graph, w);
                 }
             }
         }
 
-        public bool connected(int v, int w) {
-            return id[v] == id[w];
+        public bool connected(int v, int w)
+        {
+            return _id[v] == _id[w];
+        }
+
+        public int size(int v)
+        {
+            return _size[_id[v]];
+        }
+
+        public int id(int v)
+        {
+            return _id[v];
         }
     }
 }
